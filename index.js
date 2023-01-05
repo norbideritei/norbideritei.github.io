13b7d1df00f832fd201299763a0590528c98cfb9
Deritei Norbert <norbizeu@gmail.com>
2023-01-05 21:07:45 +0200

    Display pages with js

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-
 var skillsEl = document.getElementById("skills-list");
 
-var skills = ["HTML", "CSS", "JS","Git"];
+var skills = ["HTML", "CSS", "JS", "Git"];
 
 var skillsHTML = "";
 
-for(var i = 0; i < skills.length; i++ ) {
-    skillsHTML += "<li>" + skills[i] + "</li>";
+for (var i = 0; i < skills.length; i++) {
+  skillsHTML += "<li>" + skills[i] + "</li>";
 }
 
-skillsEl.innerHTML = skillsHTML
+skillsEl.innerHTML = skillsHTML;
 
-document.getElementById("home").style.display = 'block';
+function displayHome() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+}
+
+function displaySkills() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+}
+
+function displayLanguages() {
+  document.getElementById("languages").style.display = "block";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+}
+
+function displayProjects() {
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+}
+
+displayHome();
